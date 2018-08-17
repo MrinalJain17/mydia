6718322d81e2c8474b4b4f0e1c6f9f2626a507f7
Mrinal Jain <mrinaljain007@gmail.com>
2018-08-18 00:21:28 +0530

    Migrating docs to gh-pages (testing - 2)

--- deploy_docs.bat
+++ deploy_docs.bat
@@ -2,25 +2,26 @@
 
 cd docs
 
-echo "Deleting old documentation"
-make clean
+echo Deleting old documentation...
+call make clean
 rmdir files /S /Q
 rmdir source\auto_examples /S /Q
 git worktree prune
 rmdir .git\worktrees\files /S /Q
 
-echo "Checking out gh-pages branch into files"
+echo Checking out gh-pages branch into files...
 git worktree add -B gh-pages files origin/gh-pages
 
-echo "Removing existing files"
+echo Removing existing files...
 rmdir files\doctrees /S /Q
 rmdir files\html /S /Q
-del index.html
-del .nojekyll
+del files\index.html
+del files\.nojekyll
 
-echo "Generating site"
-make html
+echo Generating site...
+call make html
 copy index.html .\files
 copy .nojekyll .\files
 
+echo Updating gh-pages branch...
 cd files && git add --all && git commit -m "Publishing to gh-pages"
