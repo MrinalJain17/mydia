730bfb05e7d7d262e2f0b6492ca31b7f18e3697d
Mrinal Jain <mrinaljain007@gmail.com>
2018-10-15 03:47:05 +0530

    Update build-mydia.sh

--- conda_build/build-mydia.sh
+++ conda_build/build-mydia.sh
@@ -1,6 +1,6 @@
 # ---------------------------------For internal use only----------------------------------
 
-version="2.1.1"
+version="2.2.0"
 
 conda-build mydia --numpy 1.14 -c mrinaljain17 --output-folder ./dump/mydia && cd ./dump/mydia && 
 conda convert --platform win-64 "./linux-64/mydia-$version-py36h5ca1d4c_0.tar.bz2" -o . && 
