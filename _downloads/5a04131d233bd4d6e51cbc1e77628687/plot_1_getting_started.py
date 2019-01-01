0f68b49b3639d03c268440740c7e1f3efdc1f46c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-01 13:36:32 +0000

    Deploy MrinalJain17/mydia to github.com/MrinalJain17/mydia.git:gh-pages

---
+++ _downloads/5a04131d233bd4d6e51cbc1e77628687/plot_1_getting_started.py
@@ -0,0 +1,116 @@
+"""
+Get started with some basics
+============================
+
+"""
+
+######################################################################
+# 1. Read a video, given its path
+# -------------------------------
+
+# Import
+from mydia import Videos
+
+# Initialize video path
+video_path = r"./sample_video/bigbuckbunny.mp4"
+
+# Create a reader object
+reader = Videos()
+
+# Call the 'read()' function to get the video tensor
+video = reader.read(video_path)
+
+# a tensor of shape (1, 132, 720, 1280, 3)
+print("The shape of the tensor:", video.shape)
+
+######################################################################
+# The tensor can be interpreted as -
+#
+# * 1 video
+# * Having 132 frames,
+# * Dimension (width x height) of each frame: 1280x720 pixels
+# * ``3`` denotes that the video is RGB
+#
+# 2. Read multiple videos
+# -----------------------
+#
+# .. code-block:: python
+#
+#    from mydia import Videos
+#
+#    video_paths = [
+#        "path/to/video_1",
+#        "path/to/video_2",
+#        "path/to/video_3",
+#        ...,
+#    ]          # list of path of videos
+#
+#    reader = Videos()
+#    video = reader.read(video_paths)
+#
+# 3. Use multiple workers for reading the videos in parallel
+# ----------------------------------------------------------
+#
+# .. code-block:: python
+#    :emphasize-lines: 11
+#
+#    from mydia import Videos
+#
+#    video_paths = [
+#        "path/to/video_1",
+#        "path/to/video_2",
+#        "path/to/video_3",
+#        ...,
+#    ]          # list of path of videos
+#
+#    reader = Videos()
+#    video = reader.read(video_paths, workers=4)
+#
+# **The code above will use 4 CPUs to read the videos in parallel,
+# which could result in a significant speed up, depending on the
+# videos to be read and the performance of the CPU.**
+#
+# 4. Use a python generator for multiple videos
+# ---------------------------------------------
+#
+# .. code-block:: python
+#
+#    from mydia import Videos
+#
+#    video_paths = [
+#        "path/to/video_1",
+#        "path/to/video_2",
+#        "path/to/video_3",
+#        ...,
+#    ]          # list of path of videos
+#
+#    reader = Videos()
+#
+#    def generate_video():
+#        for path in video_paths:
+#            video = reader.read(path)
+#            yield video
+#
+#    for i in range(len(video_paths)):
+#        vid = next(generate_video())
+#        # Do something
+#
+# *For information on the parameters available, read the examples ahead
+# and also refer to the documentation of the class* :class:`mydia.Videos`.
+#
+#
+# Saving the loaded video tensor
+# ------------------------------
+#
+# .. important:: Once the videos have been processed, they could be
+#  saved as :obj:`numpy.ndarray` (in `.npz` or `.npy` format). For
+#  further details, view the documentation of:
+#
+#  * :obj:`numpy.save`: for saving in `.npy` format
+#
+#  * :obj:`numpy.savez`: for saving in `.npz` format
+#
+#  * :obj:`numpy.load`: for loading back the saved numpy tensors
+#
+#  Since the whole reading process is time consuming, this could turn
+#  out to be a useful way to store and reload the video tensors.
---
+++ _downloads/b850af3266f2145da2785ea836d27cbe/plot_2_frame_selection.py
@@ -0,0 +1,73 @@
+"""
+Frame selection, resizing, and grayscale conversion
+===================================================
+
+* We want to resize each frame to be 720 pixels in width and 480 pixels
+  in height.
+
+  * Set ``target_size`` to (720, 480)
+
+* All the frames are not required. Let’s just capture exactly 12  random
+  frames from the video.
+
+  * Set ``mode`` to ``"random"``
+
+* And finally, visualize the captured frames using :func:`mydia.make_grid`
+"""
+
+# Imports
+import matplotlib.pyplot as plt
+from mydia import Videos, make_grid
+
+# Initialize video path
+video_path = r"./sample_video/bigbuckbunny.mp4"
+
+# Configuring the parameters
+# For other paramaters available, view the code documentation.
+reader = Videos(target_size=(720, 480), num_frames=12, mode="random")
+
+# Call the 'read()' function to get the required video tensor
+# which will be of shape (1, 12, 480, 720, 3)
+video = reader.read(video_path)
+print("The shape of the tensor:", video.shape)
+
+# Plot the video frames in a grid
+grid = make_grid(video[0])
+plt.imshow(grid)
+
+######################################################################
+# .. note:: The number of channels for a RGB video is 3
+#  (indicated by the last value in the tuple).
+#
+#
+# * Now let's read the video with the same configuration, but in
+#   **grayscale**
+#
+#   * For this, set ``to_gray`` to `True`
+#
+# * Also, the function `make_grid()` takes certain arguments to construct
+#   the grid of frames of the video.
+#   For more info, view :func:`mydia.make_grid`.
+
+# Imports
+import matplotlib.pyplot as plt
+from mydia import Videos, make_grid
+
+# Initialize video path
+video_path = r"./sample_video/bigbuckbunny.mp4"
+
+# Configuring the parameters
+reader = Videos(target_size=(720, 480), to_gray=True, num_frames=12, mode="random")
+
+# Call the 'read()' function to get the required video tensor
+# which will be of shape (1, 12, 480, 720, 1)
+video = reader.read(video_path)
+print("The shape of the tensor:", video.shape)
+
+# Plot the video frames in a grid
+grid = make_grid(video[0], num_col=2)
+plt.imshow(grid, cmap="gray")
+
+######################################################################
+# .. note:: The number of channels for a video in gray scale is 1
+#  (indicated by the last value in the tuple).
